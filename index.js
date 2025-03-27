c6ce4e007ecf32eb2c64b46bf8ad80e31de97b86
Eden <162178069+EdenTheCoder@users.noreply.github.com>
2025-03-27 13:18:03 +0200

    vvvvvvvvvvvv

--- index.js
+++ index.js
@@ -2,10 +2,45 @@ let width = document.getElementById("width")
 let height = document.getElementById("height")
 let canvas = document.getElementById("canvas")
 
+let make = document.getElementById("make")
+
+
 function set(){
     console.log("set")
 
     canvas.style.height = height.value + "px";
     canvas.style.width = width.value + "px";
 
+}
+
+class pos{
+    x;
+    y;
+    constructor(x , y){
+        this.x = x;
+        this.y = y;
+    }
+}
+
+
+let arr = []
+
+canavs.onclick = function(e){
+    const box = element.getBoundingClientRect()
+    const xCenter = (box.left + box.right) / 2
+    const yCenter = (box.top + box.bottom) / 2
+    const xMouse = e.pageX;
+    const yMouse = e.pageY;
+    const dot = pos(xCenter-xMouse,yCenter-yMouse);
+    arr.push(dot);
+}
+
+make.onclick = function(e){
+    let str = "";
+    str += "import turtle\n"
+    str += "T = turtle.Turtle()\n"
+    for (let i = 0; i<arr.length; i++){
+        str += `T.setposition(${arr[i].x}, ${arr[i].y})\n`
+    }
+    console.log(str)
 }
