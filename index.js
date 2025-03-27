ba10335fc09f5d4a9a0a6069fcb504c9d4c5bb94
Eden <162178069+EdenTheCoder@users.noreply.github.com>
2025-03-27 12:50:00 +0200

    cccccc

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let height = document.getElementById("height")
 let canvas = document.getElementById("canvas")
 
 function set(){
-
+    console.log("set")
 
     canvas.style.height = height.value + "px";
     canvas.style.width = width.value + "px";
