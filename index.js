0735b8a0a2f4a2f6ce566b035df3d03ff046ea49
Eden <162178069+EdenTheCoder@users.noreply.github.com>
2025-03-27 12:42:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-
+let width = document.getElementById("width")
+let height = document.getElementById("height")
+let canvas = document.getElementById("canvas")
 
 function set(){
-    let width = document.getElementById("width")
-    let height = document.getElementById("height")
-    let canvas = document.getElementById("canvas")
 
-    canvas.style.height = height.innerText + "px";
-    canvas.style.width = width.innerText + "px";
+
+    canvas.style.height = height.value + "px";
+    canvas.style.width = width.value + "px";
 
 }
